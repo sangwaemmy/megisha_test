4a41d373d6df79f20a104e43a818dfc455cac965
sangwa2 <sangwa223@outlook.com>
2023-01-26 15:37:21 +0200

    web tests

---
+++ build/precache-manifest.f1358c0f618cf757b9f3cb1017117d17.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d81c6b3b287a79ac2cbafa044ca5380",
+    "url": "/react/realdeal/index.html"
+  },
+  {
+    "revision": "875e28fced6ff598a70f",
+    "url": "/react/realdeal/static/js/2.f7c4b3a5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react/realdeal/static/js/2.f7c4b3a5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c5aee9d140d031dccfe",
+    "url": "/react/realdeal/static/js/main.30b26ed3.chunk.js"
+  },
+  {
+    "revision": "efe4249b9b416f57cb3f",
+    "url": "/react/realdeal/static/js/runtime-main.524a4886.js"
+  }
+]);
